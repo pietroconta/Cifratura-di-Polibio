b025e69a3f2e0427f55fd9cc6346bef1a939318a
Pietro Contadini <famcontadini@outlook.it>
2021-04-28 19:12:33 +0200

    Aggiungere i file di progetto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿
+namespace polibio
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tab_data = new System.Windows.Forms.DataGridView();
+            this.lbl_decifrato = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.tab_data)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tab_data
+            // 
+            this.tab_data.AllowUserToAddRows = false;
+            this.tab_data.AllowUserToDeleteRows = false;
+            this.tab_data.AllowUserToOrderColumns = true;
+            this.tab_data.AllowUserToResizeColumns = false;
+            this.tab_data.AllowUserToResizeRows = false;
+            this.tab_data.BackgroundColor = System.Drawing.SystemColors.ActiveBorder;
+            this.tab_data.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tab_data.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tab_data.ColumnHeadersVisible = false;
+            this.tab_data.Location = new System.Drawing.Point(41, 102);
+            this.tab_data.Name = "tab_data";
+            this.tab_data.RowHeadersVisible = false;
+            this.tab_data.Size = new System.Drawing.Size(310, 298);
+            this.tab_data.TabIndex = 0;
+            this.tab_data.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.tab_data_CellClick);
+            // 
+            // lbl_decifrato
+            // 
+            this.lbl_decifrato.AutoSize = true;
+            this.lbl_decifrato.Location = new System.Drawing.Point(15, 404);
+            this.lbl_decifrato.Name = "lbl_decifrato";
+            this.lbl_decifrato.Size = new System.Drawing.Size(0, 13);
+            this.lbl_decifrato.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS Reference Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(75, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(182, 26);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Cifratura Polibio";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(350, 479);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbl_decifrato);
+            this.Controls.Add(this.tab_data);
+            this.Name = "Form1";
+            this.Text = "Poilibio";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.tab_data)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView tab_data;
+        private System.Windows.Forms.Label lbl_decifrato;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace polibio
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string[,] matrice = new string[6, 6];
+        string[] alfabeto = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "kq", "l", "m", "n", "o", "p", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
+
+        int i = 0;
+        int x = 0;
+        int j = 0;
+
+        int rw = 0, cl = 0;
+
+        bool dec = false, cript = false;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tab_data.RowCount = 6;
+            tab_data.ColumnCount = 6;
+
+            for (int r = 0; r < 6; r++)
+            {
+
+                for (int c = 0; c < 6; c++)
+                {
+
+                    
+                    
+                    tab_data.Rows[r].Height = 40;
+                    tab_data.Columns[c].Width = 40;
+                   
+                }
+
+            }
+
+            matrice[0, 0] = "#";
+            for (int c = 1; c < 6; c++) { x++; matrice[0, c] = x + ""; }
+            x = 0;
+            for (int r = 1; r < 6; r++) { x++; matrice[r, 0] = x + ""; }
+
+            for (int r = 1; r < 6; r++) {
+
+                for (int c = 1; c < 6; c++) {
+                 
+                    if(i < 25)
+                    matrice[r, c] = Convert.ToString(alfabeto[i]);
+                    i++;
+
+                }
+               
+            }
+
+            aggiorna();
+        }
+
+        private void aggiorna() {
+            for (int r = 0; r < 6; r++)
+            {
+                for (int c = 0; c < 6; c++)
+                {
+
+
+                    tab_data.Rows[r].Cells[c].Value = matrice[r, c];
+                }
+
+            }
+        }
+
+        private void tab_data_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+            int row = tab_data.CurrentCell.RowIndex;
+            int col = tab_data.CurrentCell.ColumnIndex;
+
+            if (row != 0 || col != 0)
+            {
+
+                if (row != 0 && col != 0)
+                {
+                    if (dec)
+                    {
+                        lbl_decifrato.Text = "";
+                        dec = false;
+                    }
+                    String decifrato = matrice[0, col] + matrice[row, 0];
+
+                    lbl_decifrato.Text += decifrato;
+                    cript = true;
+                }
+
+                else
+                {
+                    if (tab_data.CurrentCell.RowIndex != 0 && j == 0)
+                        MessageBox.Show("Seleziona prima una cella di colonna per comporre la lettera da decriptare");
+                    else if (tab_data.CurrentCell.ColumnIndex != 0 && j != 0)
+                        MessageBox.Show("Seleziona ora una cella di riga per comporre la lettera da decriptare");
+                    else
+                    {
+                        if (cript)
+                        {
+                            lbl_decifrato.Text = "";
+                            cript = false;
+                        }
+                        if (j == 0)
+                        {
+                            cl = Convert.ToInt16(matrice[tab_data.CurrentCell.RowIndex, tab_data.CurrentCell.ColumnIndex]);
+                            j++;
+                        }
+
+                        else
+                        {
+                            rw = Convert.ToInt16(matrice[tab_data.CurrentCell.RowIndex, tab_data.CurrentCell.ColumnIndex]);
+                            lbl_decifrato.Text += matrice[rw, cl];
+                            j = 0;
+                            rw = 0;
+                            cl = 0;
+                            dec = true;
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+}
